060825cfe54f3eb0ae64452d0a7f78f9d481c533
andrearigo-dev <andre.rg96@gmail.com>
2021-01-19 18:07:03 +0100

    Added adaptive histogram equalization

--- main.py
+++ main.py
@@ -21,10 +21,10 @@ img = cv.imread('dataset\\image1.jpg')
 
 
 # original image and histogram
-plt.subplot(221)
+plt.subplot(321)
 imshow(img, 'Original image')
 
-plt.subplot(222)
+plt.subplot(322)
 plt.title('Original histogram')
 color = ('b','g','r')
 for i,col in enumerate(color):
@@ -40,10 +40,10 @@ equ1[:,:,0] = cv.equalizeHist(img[:,:,0])
 equ1[:,:,1] = cv.equalizeHist(img[:,:,1])
 equ1[:,:,2] = cv.equalizeHist(img[:,:,2])
 
-plt.subplot(223)
+plt.subplot(323)
 imshow(equ1, 'Equalized image')
 
-plt.subplot(224)
+plt.subplot(324)
 plt.title('Equalized histogram')
 
 for i,col in enumerate(color):
@@ -51,5 +51,26 @@ for i,col in enumerate(color):
     plt.plot(histr,color = col)
     plt.xlim([0,256])
     
+
+# adaptive histogram equalization using CLAHE algorithm
+equ2 = np.zeros(img.shape, dtype=np.uint8)
+
+clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+
+equ2[:,:,0] = clahe.apply(img[:,:,0])
+equ2[:,:,1] = clahe.apply(img[:,:,1])
+equ2[:,:,2] = clahe.apply(img[:,:,2])
+
+plt.subplot(325)
+imshow(equ2, 'Equalized image (adaptive)')
+
+plt.subplot(326)
+plt.title('Equalized histogram (adaptive)')
+
+for i,col in enumerate(color):
+    histr = cv.calcHist([equ2],[i],None,[256],[0,256])
+    plt.plot(histr,color = col)
+    plt.xlim([0,256])
+
 
 plt.show()
