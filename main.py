9741f24561798f6a0a8f54cfd51f1ef80dd9e69a
andrearigo-dev <andre.rg96@gmail.com>
2021-01-20 11:55:03 +0100

    Removed useless plots

--- main.py
+++ main.py
@@ -34,29 +34,13 @@ def histPlot(bgr_img, title = None):
 
 img = cv.imread(os.path.join('dataset', 'image2.jpg'))
 
-fig, ax = plt.subplots(3, 2)
-fig.tight_layout(h_pad=2)
-
-# original image and histogram
-plt.subplot(321)
-imshow(img, 'Original image')
-
-plt.subplot(322)
-histPlot(img, 'Original histogram')
-
 
 # histogram equalization to improve contrast
-equ1 = np.zeros(img.shape, dtype=np.uint8)
+# equ1 = np.zeros(img.shape, dtype=np.uint8)
 
-equ1[:,:,0] = cv.equalizeHist(img[:,:,0])
-equ1[:,:,1] = cv.equalizeHist(img[:,:,1])
-equ1[:,:,2] = cv.equalizeHist(img[:,:,2])
-
-plt.subplot(323)
-imshow(equ1, 'Equalized image')
-
-plt.subplot(324)
-histPlot(equ1, 'Equalized histogram')
+# equ1[:,:,0] = cv.equalizeHist(img[:,:,0])
+# equ1[:,:,1] = cv.equalizeHist(img[:,:,1])
+# equ1[:,:,2] = cv.equalizeHist(img[:,:,2])
     
 
 # adaptive histogram equalization using CLAHE algorithm
@@ -68,11 +52,10 @@ equ2[:,:,0] = clahe.apply(img[:,:,0])
 equ2[:,:,1] = clahe.apply(img[:,:,1])
 equ2[:,:,2] = clahe.apply(img[:,:,2])
 
-plt.subplot(325)
-imshow(equ2, 'Equalized image (adaptive)')
 
-plt.subplot(326)
-histPlot(equ2, 'Equalized histogram (adaptive)')
-
-
+plot_dim = 210
+plt.subplot(plot_dim+1)
+imshow(img, 'Original image')
+plt.subplot(plot_dim+2)
+imshow(equ2, 'Adaptive histogram equalization')
 plt.show()
